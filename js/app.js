9a580f497dfbb66c7d0c8c6df7c9a6913c1737e4
Kevin <kevmartin2001@gmail.com>
2021-04-12 19:02:06 -0300

    funcion dibujarModal realizada sin exito

--- js/app.js
+++ js/app.js
@@ -59,29 +59,29 @@ function dibujarPeli(){
     infantil.innerHTML = "";
 
     for(let i in categoriaDrama){
-        let dramaHTML = `<article class="col-sm-6 col-md-4 col-lg-3" >
-        <a href="detalle.html" id="${categoriaDrama[i].codigo}" onclick="dibujarDetalle(this.id)"><img src="img/series/drama/${categoriaDrama[i].imagen}" alt="Pelicula/serie ${categoriaDrama[i].nombre}" class="imgSeries my-4"></a>
+        let dramaHTML = `<article class="col-sm-6 col-md-4 col-lg-3 my-4" >
+        <button onclick="dibujarModal(this.id)" class="m-0 p-0 imgIndex" type="button" data-bs-toggle="modal" data-bs-target="#modalDetalle" id="${categoriaDrama[i].codigo}"><img src="img/series/drama/${categoriaDrama[i].imagen}" alt="Pelicula/serie ${categoriaDrama[i].nombre}" class="imgSeries"></button>
       </article>`;
       drama.innerHTML += dramaHTML;
     }
 
     for(let i in categoriaAccion){
-        let accionHTML = `<article class="col-sm-6 col-md-4 col-lg-3">
-        <a href="detalle.html" id="${categoriaAccion[i].codigo}"><img src="img/series/drama/${categoriaAccion[i].imagen}" alt="Pelicula/serie ${categoriaAccion[i].nombre}" class="imgSeries my-4"></a>
+        let accionHTML = `<article class="col-sm-6 col-md-4 col-lg-3 my-4">
+        <button onclick="dibujarModal(this.id)" class="m-0 p-0 imgIndex" type="button" data-bs-toggle="modal" data-bs-target="#modalDetalle" id="${categoriaAccion[i].codigo}"><img src="img/series/drama/${categoriaAccion[i].imagen}" alt="Pelicula/serie ${categoriaAccion[i].nombre}" class="imgSeries"></button>
       </article>`;
       accion.innerHTML += accionHTML;
     }
 
     for(let i in categoriaComedia){
-        let comediaHTML = `<article class="col-sm-6 col-md-4 col-lg-3">
-        <a href="detalle.html" id="${categoriaComedia[i].codigo}"><img src="img/series/drama/${categoriaComedia[i].imagen}" alt="Pelicula/serie ${categoriaComedia[i].nombre}" class="imgSeries my-4"></a>
+        let comediaHTML = `<article class="col-sm-6 col-md-4 col-lg-3 my-4">
+        <button onclick="dibujarModal(this.id)" class="m-0 p-0 imgIndex" type="button" data-bs-toggle="modal" data-bs-target="#modalDetalle" id="${categoriaComedia[i].codigo}"><img src="img/series/drama/${categoriaComedia[i].imagen}" alt="Pelicula/serie ${categoriaComedia[i].nombre}" class="imgSeries"></button>
       </article>`;
       comedia.innerHTML += comediaHTML;
     }
 
     for(let i in categoriaInfantiles){
-        let infantilHTML = `<article class="col-sm-6 col-md-4 col-lg-3">
-        <a href="detalle.html" id="${categoriaInfantiles[i].codigo}"><img src="img/series/drama/${categoriaInfantiles[i].imagen}" alt="Pelicula/serie ${categoriaInfantiles[i].nombre}" class="imgSeries my-4"></a>
+        let infantilHTML = `<article class="col-sm-6 col-md-4 col-lg-3 my-4">
+        <button onclick="dibujarModal(this.id)" class="m-0 p-0 imgIndex" type="button" data-bs-toggle="modal" data-bs-target="#modalDetalle" id="${categoriaInfantiles[i].codigo}"><img src="img/series/drama/${categoriaInfantiles[i].imagen}" alt="Pelicula/serie ${categoriaInfantiles[i].nombre}" class="imgSeries"></button>
       </article>`;
       infantil.innerHTML += infantilHTML;
     }
@@ -89,3 +89,28 @@ function dibujarPeli(){
 }
 
 
+// funcion para escribir los datos del objeto pelicula en el modal, aunque sin exito :(
+
+function dibujarModal(){
+    let modalDetalle = document.getElementById('seccionDetalle');
+    modalDetalle.innerHTML = "";
+    for (let i in listaPelicula){
+        let detalleHTML = `<img src="img/series/detalle/${listaPelicula[i].imagen}" class="imagenDetalle" alt="portada ---">
+        <div class="contenedorDetalle2">
+            <h2>${listaPelicula[i].nombre}</h2>
+            <div class="row py-5">
+                <article class="col-lg-8 col-md-12">
+                    <p>${listaPelicula[i].descripcion}</p>
+                </article>
+                <article class="col-lg-4 col-md-12 contBoton">
+                    <a href="error404.html"><button type="button" class="btReproducir">Reproducir</button></a>
+                </article>
+            </div>
+            <h3>Trailer</h3>
+            ${listaPelicula[i].video}
+        </div>`
+
+        modalDetalle.innerHTML = detalleHTML;
+    }
+
+}
